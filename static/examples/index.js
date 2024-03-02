7f329eb10e40fac052c7047f2c4c511d470fc31f
Abbas Majidov <69363088+absmj@users.noreply.github.com>
2024-03-02 22:05:25 +0400

    Update index.js

--- static/examples/index.js
+++ static/examples/index.js
@@ -15,5 +15,17 @@ const examples = [
         type: 'text/xml',
         name: 'p5js.svg',
         url: '/static/examples/p5js.svg'
+    },
+    
+    {
+        type: 'text/xml',
+        name: 'i-en.svg',
+        url: '/static/examples/i-en.svg'
+    },
+
+    {
+        type: 'text/xml',
+        name: 'i-az.svg',
+        url: '/static/examples/i-az.svg'
     }
 ]
